4016586db2acc34effbc87e858ec7cd4d57b16b5
subhankar-navsoft <subhankar.chaulay@navsoft.in>
2020-09-17 19:33:31 +0530

    changes for profile details

--- src/app/components/dashboard/operations/operations-accreditation-service-details/operations-accreditation-service-details.component.ts
+++ src/app/components/dashboard/operations/operations-accreditation-service-details/operations-accreditation-service-details.component.ts
@@ -17,6 +17,12 @@ export class OperationsAccreditationServiceDetailsComponent implements OnInit {
   loader:boolean = false;
   subscriptions: Subscription[] = [];
   routeId:any;
+  serviceDetail:any[] = [];
+  ownershipOfOrg:any;
+  bodMember:any;
+  otherAccr:any;
+  other_accr_model:any;
+  Object = Object;
 
   constructor(private _service: AppService, private _constant: Constants, public _toaster: ToastrService,
     private _trainerService: TrainerService) { }
@@ -27,11 +33,28 @@ export class OperationsAccreditationServiceDetailsComponent implements OnInit {
     this.loadData();
   }
 
+  jsonParse(data) {
+    // console.log(data);
+    // console.log(JSON.parse(data.toString()));
+    // return JSON.parse(data.toString());
+    // var obj = "{'include':true,'fn':'1-12'}"; //Assuming double quotes outside
+    var obj1 = data.replace(/'/g, "\""); //Replace single quotes with double quotes
+    // console.log(typeof obj1); // string
+
+    // var myjsonobj = JSON.parse(obj1); //convert to JSON
+    return Array(JSON.parse(obj1));
+  }
+
   loadData() {
     this.subscriptions.push(this._trainerService.trainerAccredDetailsServtrainerAccredDetailsServ(this.routeId)
           .subscribe(
             result => {
-              console.log(result);
+              // console.log(result);
+              this.serviceDetail = result['data']
+              this.ownershipOfOrg = result['data']['ownershipOfOrg']
+              this.bodMember = result['data']['bodMember'];
+              this.otherAccr = result['data']['otherAccr'];
+              this.other_accr_model = result['data']['otherAccr'] != '' ? '1' : '0' ;
             },
             ()=>{
               console.log('comp...');
