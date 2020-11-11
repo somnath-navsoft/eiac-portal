3e072a8e666b795bcf924f167eb023ed87417918
milan-navsoft <milan.maiti@navsoft.in>
2020-11-11 18:31:56 +0530

    msg

--- src/app/components/dashboard/operations/operations-message-details/operations-message-details.component.ts
+++ src/app/components/dashboard/operations/operations-message-details/operations-message-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { Constants } from 'src/app/services/constant.service';
+import { AppService } from 'src/app/services/app.service';
+import { ToastrService } from 'ngx-toastr';
+import  { UiDialogService } from  'src/app/services/uiDialog.service';
 
 @Component({
   selector: 'app-operations-message-details',
@@ -7,9 +12,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationsMessageDetailsComponent implements OnInit {
 
-  constructor() { }
+  userType:any;
+  userEmail:any;
+  routeId:any;
+  cabUserDetails:any;
+  cabStep1:any;
+  cabStep2:any;
+  tabId:any = 'personal_details';
+  approveRejectStatus:any = '';
+  loader:boolean = true;
+  tradeLicenseFile:any;
+  tradeLicenseText:any;
+  rejectedMessageId:boolean = false;
+
+  constructor(public Service: AppService, public constant:Constants,public router: Router,public route: ActivatedRoute,public toastr: ToastrService,public uiDialog: UiDialogService) { }
 
   ngOnInit() {
+    this.userType = sessionStorage.getItem('type');
+    this.userEmail = sessionStorage.getItem('email');
+    // this.routeId = this.route.snapshot.queryParamMap.get('id');
+    // console.log(sessionStorage.getItem('routeId'));
+    this.routeId = sessionStorage.getItem('routeId');
+
+    this.userType = sessionStorage.getItem('type');
+    if(this.userType != 'operations')
+    {
+      var landUrl = '/dashboard'+this.userType+'/home'
+      this.router.navigateByUrl(landUrl);
+    }
+    this.loadData()
+  }
+
+  loadData() {
+    this.loader = false;
+    this.Service.getwithoutData(this.Service.apiServerUrl+"/"+this.constant.API_ENDPOINT.profileService+'?id='+this.routeId)
+    .subscribe(
+      res => {
+        this.loader = true;
+        this.cabUserDetails = res['data']['user_data'][0];
+        this.approveRejectStatus = res['data']['user_data'][0].approved;
+        this.cabStep1 = res['data']['step1'][0];
+        this.cabStep2 = res['data']['step2'];
+        this.tradeLicenseFile = this.constant.mediaPath+this.cabStep1.trade_license
+        var tradeLicenseField = this.cabStep1.trade_license != null ? this.cabStep1.trade_license.split('/') : '';
+        this.tradeLicenseText = tradeLicenseField[4];
+    });
+  }
+
+  tabClick(id) {
+    this.tabId = id;
+    if(this.tabId == 'application_info') {
+      var element = document.querySelector('#'+this.tabId);
+      element.classList.remove("tab-active");
+      
+
+      var element2 = document.querySelector('#personal_details');
+      element2.classList.add("tab-active");
+    }else if(this.tabId == 'personal_details') {
+      var element = document.querySelector('#'+this.tabId);
+      element.classList.remove("tab-active");
+
+      var element2 = document.querySelector('#application_info');
+      element2.classList.add("tab-active");
+    }
+  }
+
+  approveOrReject(status) {
+    if(status == 'approve') {
+      this.Service.getwithoutData(this.Service.apiServerUrl+"/"+this.constant.API_ENDPOINT.profileApproval+'?id='+this.routeId+'&status=1')
+      .subscribe(
+        res => {
+          this.approveRejectStatus = '1';
+      });
+      
+    }
+  }
+
+  openRejectedPop() {
+    this.rejectedMessageId = true;
+  }
+
+  getAllData(event) {
+    // console.log(event);
+    if(event.rejectId == 2) {
+      this.Service.getwithoutData(this.Service.apiServerUrl+"/"+this.constant.API_ENDPOINT.profileApproval+'?id='+this.routeId+'&status=2'+'&message='+event.message)
+      .subscribe(
+        res => {
+          this.toastr.success('Message sent successfully', '');
+          this.approveRejectStatus = '2';
+      });
+      
+    }
   }
 
 }
