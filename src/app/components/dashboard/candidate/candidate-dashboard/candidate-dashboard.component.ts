a358a25090f3144d988a3545ad1247aedd01a586
milan-navsoft <milan.maiti@navsoft.in>
2020-11-20 19:43:33 +0530

    message section

--- src/app/components/dashboard/candidate/candidate-dashboard/candidate-dashboard.component.ts
+++ src/app/components/dashboard/candidate/candidate-dashboard/candidate-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Constants } from 'src/app/services/constant.service';
+import { AppService } from 'src/app/services/app.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-candidate-dashboard',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateDashboardComponent implements OnInit {
 
-  constructor() { }
+  messageList: any;
+  userId: any;
+  loader: boolean = true;
+  recordsTotal: any;
+
+  constructor(public Service: AppService, public constant: Constants, public router: Router, public toastr: ToastrService) { }
 
   ngOnInit() {
+    this.userId = sessionStorage.getItem('userId');
+
+    this.loader = false;
+    this.Service.getwithoutData(this.Service.apiServerUrl + "/" + this.constant.API_ENDPOINT.messageList + '?id=' + this.userId)
+      .subscribe(
+        res => {
+          this.messageList = res['data'].message_list;
+          this.recordsTotal = res['data'].recordsTotal;
+          this.loader = true;          
+        });
+  }
+
+  getUserDetails(user){
+    sessionStorage.setItem('messageUserDetails', JSON.stringify(user));
   }
 
 }
