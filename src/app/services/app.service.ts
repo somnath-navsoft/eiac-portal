5a7c01bc774d0e448cdcf3ffb479a3ad01a71ffb
Somnath Karmokar <somnath.karmokar@flavoursclub.in>
2020-11-27 10:43:31 +0530

    merge

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -25,8 +25,8 @@ export class AppService {
   // public apiServerUrl         =   'https://uat-service.eiac.gov.ae/webservice';
   // public apiRequestUrl        =   'https://uat-service.eiac.gov.ae/';
 
-  public apiServerUrl         =   'https://dev-service.eiac.gov.ae/webservice';
-  public apiRequestUrl        =   'https://dev-service.eiac.gov.ae/';
+  public apiServerUrl         =   'https://uat-service.eiac.gov.ae/webservice';
+  public apiRequestUrl        =   'https://uat-service.eiac.gov.ae/';
 
   public apiUatServerUrl         =   'https://uat-service.eiac.gov.ae/webservice';
   // public apiRequestUrl        =   'https://uat-service.eiac.gov.ae/';
