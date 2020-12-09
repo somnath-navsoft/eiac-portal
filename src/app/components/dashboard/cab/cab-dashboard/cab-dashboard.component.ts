4db7ad96b24877cd7f6cd9a5ad4e00564dd46394
milan-navsoft <milan.maiti@navsoft.in>
2020-12-09 17:38:15 +0530

    msg

--- src/app/components/dashboard/cab/cab-dashboard/cab-dashboard.component.ts
+++ src/app/components/dashboard/cab/cab-dashboard/cab-dashboard.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Constants } from 'src/app/services/constant.service';
 import { AppService } from 'src/app/services/app.service';
 import { ToastrService } from 'ngx-toastr';
 import { ignoreElements } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { FormControl } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cab-dashboard',
@@ -22,6 +27,35 @@ export class CabDashboardComponent implements OnInit {
   recordsTotal: any;
   config: any;
 
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruitCtrl = new FormControl();
+  filteredFruits: Observable<string[]>;
+  fruits: string[] = [];
+  allFruits: string[] = [];
+  addOnBlur = true;
+  select_field: any = [];
+  selectedUser: any = [];
+
+  selectedField: any = 'Internal Operations';
+  getUserType: string = 'internal_operations';
+  document: any = '';
+  documentName: any = '';
+  searchDetails: any = [];
+  selectSearch: any = [];
+  selectedUserId: any = [];
+  chatMessage: any = {};
+  file_validation: boolean = true;
+  chatMessageFile: any = new FormData();
+  localUrl: any;
+  button_disable: any = true;
+  @ViewChild('fruitInput', { static: false }) fruitInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto', { static: false }) matAutocomplete: MatAutocomplete;
+  
+
   constructor(public Service: AppService, public constant: Constants, public router: Router, public toastr: ToastrService) {
     this.config = {
       itemsPerPage: this.Service.dashBoardPagination,
@@ -30,6 +64,13 @@ export class CabDashboardComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    this.select_field = [
+      { field: 'Internal Operations', value: 'Internal Operations' },
+      { field: 'Trainer', value: 'Trainer' }      
+    ];
+
+
     this.userEmail = sessionStorage.getItem('email');
     this.userType = sessionStorage.getItem('type');
     this.userId = sessionStorage.getItem('userId');
@@ -63,6 +104,172 @@ export class CabDashboardComponent implements OnInit {
 
   pageChanged(event) {
     this.config.currentPage = event;
+  }
+
+  onSubmit(ngForm) {
+
+    if (ngForm.form.valid && this.selectedUserId != '') {
+      let formdata = new FormData();
+      formdata.append('message_by', this.userId);
+      formdata.append('user_id', this.selectedUserId);
+      // formdata.append('message_id', null);
+      formdata.append('message', this.chatMessage.message);
+      formdata.append('document', this.document);
+      formdata.append('message_type', 'admin_comment');
+      this.loader = false;
+      this.Service.put(this.Service.apiServerUrl + "/" + 'message-list/', formdata)
+        .subscribe(
+          res => {
+            if (res['status'] == true) {
+              this.setField('Internal Operations');
+              this.documentName = '';
+              this.selectedUserId = '';
+              this.selectedUser = [];
+              this.chatMessage.message = '';
+              this.chatMessage.upload_message = '';
+              this.loader = true;
+              this.toastr.success(res['msg'], '');
+              // this.getMessage();
+            }
+          }, err => {
+            this.loader = true;
+          })
+    }
+  }
+
+  setField(value) {
+    // this.search(this.searchTerm);
+    this.selectedUser = [];
+    this.loader = false;
+    this.searchDetails = [];
+    this.selectSearch = [];
+    this.selectedField = value;
+    // cab_code,cab_client,candidate,assessors,trainers,super_admin
+    if (this.selectedField == 'CAB Name') {
+      this.getUserType = 'cab_client';
+    }
+    if (this.selectedField == 'CAB Code') {
+      this.getUserType = 'cab_code';
+    }
+    if (this.selectedField == 'Candidate') {
+      this.getUserType = 'candidate';
+    }
+    if (this.selectedField == 'Trainer') {
+      this.getUserType = 'trainers';
+    }
+    if (this.selectedField == 'Assessor') {
+      this.getUserType = 'assessors';
+    }
+    if (this.selectedField == 'Super Admin') {
+      this.getUserType = 'super_admin';
+    }
+    if (this.selectedField == 'Internal Operations') {
+      this.getUserType = 'internal_operations';
+    }
+
+    this.Service.getwithoutData(this.Service.apiServerUrl + "/" + 'message-user-list' + '?type=' + this.getUserType + '&searchKey=S')
+      .subscribe(
+        res => {
+          this.searchDetails = res['data'].user_list;
+          this.loader = true;;
+          // this.search(this.searchTerm);
+
+        }, err => {
+          this.loader = true;
+        });
+
+  }
+
+  showFile() {
+    window.open(this.localUrl, '_blank');
+  }
+  
+
+  add(event: MatChipInputEvent): void {
+
+  }
+
+  remove(fruit: any): void {
+    this.button_disable = true;
+    this.selectedUser = [];
+  }
+
+  getFileName(file) {
+    return file.split('/')[-1];
+  }
+
+  search(query: string) {
+    // this.searchTerm = query;
+    let result = this.select(query);
+    // this.searchDetails = result;
+    if (query != '') {
+      this.selectSearch = result;
+    } else {
+      this.selectSearch = [];
+    }
+
+  }
+
+  select(query: string): string[] {
+    let result: string[] = [];
+    if (this.getUserType == 'cab_client' || this.getUserType == 'cab_code') {
+      for (let a of this.searchDetails) {
+        if (a.username.toLowerCase().indexOf(query) > -1) {
+          result.push(a);
+        }
+      }
+    } else {
+      for (let a of this.searchDetails) {
+        if (a.email.toLowerCase().indexOf(query) > -1) {
+          result.push(a);
+        }
+      }
+    }
+
+    // this.searchDetails = result;
+    return result;
+  }
+
+
+  getValue(value, data) {
+    this.fruitInput.nativeElement.blur();
+    this.selectedUser = [];
+    this.selectedUserId = value.id;
+    this.button_disable = this.selectedUserId != '' ? false : true;
+    if (data == 'username') {
+      this.selectedUser.push(value.username);
+    } else {
+      this.selectedUser.push(value.email);
+    }
+    this.fruitInput.nativeElement.value = '';
+    this.fruitInput.nativeElement.blur();
+
+
+  }
+
+
+  validateFile(fileEvent: any) {
+    // window.open(fileEvent.target.value, '_blank');
+    this.localUrl = window.URL.createObjectURL(fileEvent.target.files[0]);
+    this.document = fileEvent.target.files[0];
+    var file_name = fileEvent.target.files[0].name;
+    var file_exe = file_name.substring(file_name.lastIndexOf('.') + 1, file_name.length);
+    var ex_type = ['pdf'];
+    var ex_check = this.Service.isInArray(file_exe, ex_type);
+    if (ex_check) {
+      this.chatMessage.upload_message = fileEvent.target.files[0].name;
+      this.chatMessageFile.append('upload_message_file', fileEvent.target.files[0]);
+      this.file_validation = true;
+      this.documentName = fileEvent.target.files[0].name;
+      return true;
+    } else {
+      this.file_validation = false;
+      this.documentName = '';
+      this.document = '';
+      return false;
+    }
+
+
   }
 
 }
