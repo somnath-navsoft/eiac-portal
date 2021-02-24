f34234f189e672c2d4c822cdbc47ea68adc2ea33
subhankar-navsoft <subhankar.chaulay@navsoft.in>
2021-02-24 17:31:22 +0530

    update

--- src/app/components/dashboard/cab/application-accreditation/halal-conformity-form/halal-conformity-form.component.ts
+++ src/app/components/dashboard/cab/application-accreditation/halal-conformity-form/halal-conformity-form.component.ts
@@ -1389,8 +1389,10 @@ addSchemeRow(obj: any = [],index: number){
               this.selectTradeLicName = getFile[4].toString().split('.')[0];
               this.selectTradeLicPath = this.constant.mediaPath +  data.trade_license.toString();
             }
+            this.step1Data.trade_license_number = data.trade_license_number;
+            this.step1Data.trade_license        = data.trade_license;
           }
-          this.step1Data.trade_license_number = data.trade_license_number;
+          
           
           this.step1Data.city =  data.city;
           this.step1Data.country = data.country;
